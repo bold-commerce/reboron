043146f4eaa3e8d99fafdb5251c1c8812f0c11a3
Brandon Wade <brandon_wade2005@hotmail.com>
2017-09-05 00:47:02 -0500

    Update DropModal keyframes to use emotion.

--- src/DropModal.js
+++ src/DropModal.js
@@ -1,72 +1,73 @@
 import modalFactory from './modalFactory';
 import insertKeyframesRule from 'domkit/insertKeyframesRule';
 import appendVendorPrefix from 'domkit/appendVendorPrefix';
+import { keyframes } from 'emotion';
 
 const animation = {
-  show: {
-    animationDuration: '0.4s',
-    animationTimingFunction: 'cubic-bezier(0.7,0,0.3,1)',
-  },
-  hide: {
-    animationDuration: '0.4s',
-    animationTimingFunction: 'cubic-bezier(0.7,0,0.3,1)',
-  },
-  showModalAnimation: insertKeyframesRule({
-    '0%': {
-      opacity: 0,
-      transform: 'translate(-50%, -300px)',
+    show: {
+        animationDuration: '0.4s',
+        animationTimingFunction: 'cubic-bezier(0.7, 0, 0.3, 1)',
     },
-    '100%': {
-      opacity: 1,
-      transform: 'translate(-50%, -50%)',
+    hide: {
+        animationDuration: '0.4s',
+        animationTimingFunction: 'cubic-bezier(0.7, 0, 0.3, 1)',
     },
-  }),
-  hideModalAnimation: insertKeyframesRule({
-    '0%': {
-      opacity: 1,
-      transform: 'translate(-50%, -50%)',
-    },
-    '100%': {
-      opacity: 0,
-      transform: 'translate(-50%, 100px)',
-    },
-  }),
-  showBackdropAnimation: insertKeyframesRule({
-    '0%': {
-      opacity: 0,
-    },
-    '100%': {
-      opacity: 0.9,
-    },
-  }),
-  hideBackdropAnimation: insertKeyframesRule({
-    '0%': {
-      opacity: 0.9,
-    },
-    '100%': {
-      opacity: 0,
-    },
-  }),
-  showContentAnimation: insertKeyframesRule({
-    '0%': {
-      opacity: 0,
-      transform: 'translate(0, -20px)',
-    },
-    '100%': {
-      opacity: 1,
-      transform: 'translate(0, 0)',
-    },
-  }),
-  hideContentAnimation: insertKeyframesRule({
-    '0%': {
-      opacity: 1,
-      transform: 'translate(0, 0)',
-    },
-    '100%': {
-      opacity: 0,
-      transform: 'translate(0, 50px)',
-    },
-  }),
+    showModalAnimation: keyframes`
+        0% {
+            opacity: 0;
+            transform: translate(-50%, -300px);
+        }
+        100% {
+            opacity: 1;
+            transform: translate(-50%, -50%);
+        }
+    `,
+    hideModalAnimation: keyframes`
+        0% {
+            opacity: 1;
+            transform: translate(-50%, -50%);
+        }
+        100% {
+            opacity: 0;
+            transform: translate(-50%, 100px);
+        }
+    `,
+    showBackdropAnimation: keyframes`
+        0% {
+            opacity: 0;
+        }
+        100% {
+            opacity: 0.9;
+        }
+    `,
+    hideBackdropAnimation: keyframes`
+        0% {
+            opacity: 0.9;
+        }
+        100% {
+            opacity: 0;
+        }
+    `,
+    showContentAnimation: keyframes`
+        0% {
+            opacity: 0;
+            transform: translate(0, -20px);
+        }
+        100% {
+            opacity: 1;
+            transform: translate(0, 0);
+        }
+    `,
+    hideContentAnimation: keyframes`
+        0% {
+            opacity: 1;
+            transform: translate(0, 0);
+        }
+        100% {
+            opacity: 0;
+            transform: translate(0, 50px);
+        }
+    `,
 };
 
 const showAnimation = animation.show;
@@ -79,48 +80,48 @@ const showContentAnimation = animation.showContentAnimation;
 const hideContentAnimation = animation.hideContentAnimation;
 
 export default modalFactory({
-  getRef: (closing) => {
-    return 'modal';
-  },
-  getModalStyle: (closing) => {
-    return appendVendorPrefix({
-      position: 'fixed',
-      width: '500px',
-      transform: 'translate(-50%, -50%)',
-      top: '50%',
-      left: '50%',
-      backgroundColor: 'white',
-      zIndex: 1050,
-      animationDuration: (closing ? hideAnimation : showAnimation).animationDuration,
-      animationFillMode: 'forwards',
-      animationName: closing ? hideModalAnimation : showModalAnimation,
-      animationTimingFunction: (closing ? hideAnimation : showAnimation).animationTimingFunction,
-    });
-  },
-  getBackdropStyle: (closing) => {
-    return appendVendorPrefix({
-      position: 'fixed',
-      top: 0,
-      right: 0,
-      bottom: 0,
-      left: 0,
-      zIndex: 1040,
-      backgroundColor: '#373A47',
-      animationDuration: (closing ? hideAnimation : showAnimation).animationDuration,
-      animationFillMode: 'forwards',
-      animationName: closing ? hideBackdropAnimation : showBackdropAnimation,
-      animationTimingFunction: (closing ? hideAnimation : showAnimation).animationTimingFunction,
-    });
-  },
-  getContentStyle: (closing) => {
-    return appendVendorPrefix({
-      margin: 0,
-      opacity: 0,
-      animationDuration: (closing ? hideAnimation : showAnimation).animationDuration,
-      animationFillMode: 'forwards',
-      animationDelay: '0.25s',
-      animationName: showContentAnimation,
-      animationTimingFunction: (closing ? hideAnimation : showAnimation).animationTimingFunction,
-    });
-  },
+    getRef: (closing) => {
+        return 'modal';
+    },
+    getModalStyle: (closing) => {
+        return appendVendorPrefix({
+            position: 'fixed',
+            width: '500px',
+            transform: 'translate(-50%, -50%)',
+            top: '50%',
+            left: '50%',
+            backgroundColor: 'white',
+            zIndex: 1050,
+            animationDuration: (closing ? hideAnimation : showAnimation).animationDuration,
+            animationFillMode: 'forwards',
+            animationName: closing ? hideModalAnimation : showModalAnimation,
+            animationTimingFunction: (closing ? hideAnimation : showAnimation).animationTimingFunction,
+        });
+    },
+    getBackdropStyle: (closing) => {
+        return appendVendorPrefix({
+            position: 'fixed',
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+            zIndex: 1040,
+            backgroundColor: '#373A47',
+            animationDuration: (closing ? hideAnimation : showAnimation).animationDuration,
+            animationFillMode: 'forwards',
+            animationName: closing ? hideBackdropAnimation : showBackdropAnimation,
+            animationTimingFunction: (closing ? hideAnimation : showAnimation).animationTimingFunction,
+        });
+    },
+    getContentStyle: (closing) => {
+        return appendVendorPrefix({
+            margin: 0,
+            opacity: 0,
+            animationDuration: (closing ? hideAnimation : showAnimation).animationDuration,
+            animationFillMode: 'forwards',
+            animationDelay: '0.25s',
+            animationName: showContentAnimation,
+            animationTimingFunction: (closing ? hideAnimation : showAnimation).animationTimingFunction,
+        });
+    },
 });
